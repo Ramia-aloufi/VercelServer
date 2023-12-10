2a3afe02c934aef605513eb284f623671177766d
Ramia-aloufi <ramia.aloufi@gmail.com>
2023-12-11 02:15:33 +0300

    modify src

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ export const dev = {
             port:process.env.PORT || 8000
         },
         db:{
-            url:process.env.MONGO_URL || 'mongodb+srv://ramia:12312344@cluster0.tcv9vml.mongodb.net/'
+            url:process.env.MONGO_URL || 'mongodb+srv://ramia:12312344@cluster0.tcv9vml.mongodb.net/test'
 
         }
     
